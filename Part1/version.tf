9a622451a920f523096edb403b4ba6e68ade16e3
Agamya Mehrotra <agamyamehrotra@Agamyas-MacBook-Air.local>
2025-09-05 15:53:11 +0530

    Fixing version.tf

--- Part1/version.tf
+++ Part1/version.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = "your_access_key"
-  secret_key = "your_secret_key"
+  # access_key = "<<your_access_key>>"
+  # secret_key = "<<your_secret_key>>"
 }
 
