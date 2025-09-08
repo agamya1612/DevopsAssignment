c5e53a6ac3f32e1801a1bb965cf9cc233ab0cb9b
Agamya Mehrotra <agamyamehrotra@Agamyas-MacBook-Air.local>
2025-09-08 23:40:14 +0530

    Complete DevOps Assignment (Terraform + ECS + Node.js)

---
+++ Part1/version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+ #  access_key = "<<Give your own access key>>"
+  #  secret_key = "<<Give your own secret key>>"
+}
+
--- Part4/outputs.tf
+++ Part4/outputs.tf
@@ -9,3 +9,7 @@ output "ecs_log_group" {
 output "sns_topic_arn" {
   value = aws_sns_topic.alerts.arn
 }
+
+output "sns_topic_name" {
+  value = aws_sns_topic.alerts.name
+}
